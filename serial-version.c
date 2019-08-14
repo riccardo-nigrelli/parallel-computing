9ed451e9543ae718525d46fdd7ec3359ca29e422
Riccardo Nigrelli <20014421@studenti.uniupo.it>
2019-08-14 11:06:48 +0200

    serial version pi evaluation

---
+++ serial-version.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+#define SEED time(NULL)
+
+double pi_serial_version(const int interval) {
+  
+  srand(SEED);
+
+  double x, y;
+  int i, circle = 0;
+  
+  for (i = 0; i < interval; i ++) {
+    x = rand() / (double) RAND_MAX;
+    y = rand() / (double) RAND_MAX;
+    
+    if (x * x + y * y <= 1.0) circle ++;
+  }
+
+  return (double) circle / interval * 4.0;
+}
+
+int main(void) {
+
+  clock_t start, end;
+
+  start = clock();
+  printf("\u03C0 \u2248 %f\n", pi_serial_version(1e9));
+  end = clock();
+  printf("Tempo versione seriale: %.5f\n", (double)(end - start) / CLOCKS_PER_SEC);
+
+  return 0;
+}

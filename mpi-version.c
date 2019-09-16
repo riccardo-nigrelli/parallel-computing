3f2c902dd8fbfe5776041e07fb594ba57e678962
Riccardo Nigrelli <20014421@studenti.uniupo.it>
2019-09-16 15:28:45 +0200

    resolved write file error

--- mpi-version.c
+++ mpi-version.c
@@ -6,12 +6,15 @@
 
 #define SEED time(NULL)
 
-void pi_mpi_version(int argc, char **argv) {
+int main(int argc, char **argv) {
 
   int rank, size;
   unsigned int seed;
   double x, y, start, end;
-  long long int i, all_point, points = 0, all_intern;
+  long long int i, j, all_point, points, all_intern;
+
+  FILE *fp = fopen("simulation-mpi.txt", "w");
+  long long int dim[] = {5, 50, 500, 5000, 50000, 500000, 5000000, 50000000};
 
   MPI_Init(&argc, &argv);
   start = MPI_Wtime();
@@ -21,33 +24,40 @@ void pi_mpi_version(int argc, char **argv) {
 
   seed = SEED + rank;
 
-  for ( i = 0; i < atoll(argv[1]); i++ ) {
-    x = (double) rand_r(&seed) / RAND_MAX;
-    y = (double) rand_r(&seed) / RAND_MAX;
+  for ( j = 0; j < 8; j++ ) {
+    points = 0;
 
-    if ( x * x + y * y <= 1.0 ) points++;
-  }
+    for ( i = 0; i < dim[j]; i++ ) {
+      x = (double) rand_r(&seed) / RAND_MAX;
+      y = (double) rand_r(&seed) / RAND_MAX;
 
-  MPI_Reduce(&points, &all_intern, 1, MPI_LONG_LONG_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-  all_point = atoll(argv[1]) * size;
+      if ( x * x + y * y <= 1.0 ) points++;
+    }
 
-  MPI_Barrier(MPI_COMM_WORLD);
-  end = MPI_Wtime();
+    MPI_Reduce(&points, &all_intern, 1, MPI_LONG_LONG_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    all_point = dim[j] * size;
 
-  if ( rank == 0 ) {
-    printf("\u03C0 \u2248 %Lf\n", (long double) all_intern / all_point * 4.0);
-    printf("Time elapsed: %.4f\n", end - start);
+    MPI_Barrier(MPI_COMM_WORLD);
+    end = MPI_Wtime();
+
+    if ( rank == 0 ) {
+      printf("\u03C0 \u2248 %Lf\n", (long double) all_intern / all_point * 4.0);
+      printf("Time elapsed: %.4f\n", end - start);
+      fprintf(fp, "%lld\t%Lf\t%.4f\n", all_point, (long double) all_intern / all_point * 4.0, end - start);
+    }
   }
 
   MPI_Finalize();
-}
-
-int main(int argc, char **argv) {
-  
-  if ( argc == 2 )
-    pi_mpi_version(argc, argv);
-  else 
-    printf("Error, specify the number of sample to be generate");
-
+  fclose(fp);
   return 0;
 }
+
+// int main(int argc, char **argv) {
+  
+//   pi_mpi_version(argc, argv);
+//   if ( argc == 2 )
+//   else 
+//     printf("Error, specify the number of sample to be generate");
+
+//   return 0;
+// }
